3b8022742a9831620c0cfbe0249bce3f6964078f
Yvonne Hayes <queyvonne@gmail.com>
2016-03-22 10:31:15 -0700

    Got basic table and random pizza number

---
+++ app.js
@@ -0,0 +1,29 @@
+//Random Number Generator
+
+function randomNumber(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+//Number of Delivery runs
+
+function randomNumberD() {
+  return
+}
+
+
+// Store Objects
+var Beaverton = {
+
+
+}
+ // Time  | Min Pizza/hr  | Max Pizza/hr | Min Delivery/hr | Max Delivery/hr  |
+
+var printIt = document.getElementById('test');
+printIt.textContent = "hi";
+
+var printBPN8 = document.getElementById('BPN8');
+printBPN8.textContent = randomNumber(1,4);
+
+// var printBDN8 = document.getElementById('BDN8');
+// printBDN8.textContent = randomNumber(1,4) <=
