cea9e7b8da13e07c39f4f537aaf395698b19fc8a
Yvonne Hayes <queyvonne@gmail.com>
2016-03-22 10:53:38 -0700

    Recommended Number of Drivers working

--- app.js
+++ app.js
@@ -5,17 +5,7 @@ function randomNumber(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-
-
-// Store Objects
-var Beaverton = {
-
-
-}
- // Time  | Min Pizza/hr  | Max Pizza/hr | Min Delivery/hr | Max Delivery/hr  |
-
-var printIt = document.getElementById('test');
-printIt.textContent = "hi";
+// Figuring out how to get correct random numbers
 
 var printBPN8 = document.getElementById('BPN8');
 var BPN8am = randomNumber(0,4)
@@ -26,5 +16,6 @@ var printBDN8 = document.getElementById('BDN8');
 var BDN8am = Math.ceil(BPN8am/2);
 printBDN8.textContent = BDN8am;
 
-// var printBDN8 = document.getElementById('BDN8');
-// printBDN8.textContent = randomNumber(1,4) <=
+var printBRD8 = document.getElementById('BRD8');
+var BRD8am = Math.ceil(BDN8am/3);
+printBRD8.textContent = BRD8am;
