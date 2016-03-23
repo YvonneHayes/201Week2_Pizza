2a001162beefe4e55c92655624e8609eba34f097
Yvonne Hayes <queyvonne@gmail.com>
2016-03-23 11:21:51 -0700

    Three Objects and tables up and running

--- app3.js
+++ app3.js
@@ -1,26 +1,31 @@
 //Beaverton Object
 
 var Beaverton = {
+  AnchorName: 'BeavertonAnchor',
   timeSlots: ['8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12am','1am'],
   minMaxPizza: [[0,4],[0,4],[0,4],[0,7],[0,7],[0,7],[2,15],[2,15],[2,15],[15,35],[15,35],[15,35],[12,31],[12,31],[12,31],[5,20],[5,20],[5,20]],
-  pizzaSales: ['BPizzaSales8','BPizzaSales9','BPizzaSales10','BPizzaSales11','BPizzaSales12','BPizzaSales13','BPizzaSales14','BPizzaSales15','BPizzaSales16','BPizzaSales17','BPizzaSales18','BPizzaSales19','BPizzaSales20','BPizzaSales21','BPizzaSales22','BPizzaSales23','BPizzaSales24','BPizzaSales1'],
-  minMaxDeliveries: [[0,4],[0,4],[0,4],[0,4],[0,4],[0,4],[1,4],[1,4],[1,4],[3,8],[3,8],[3,8],[5,12],[5,12],[5,12],[6,11],[6,11],[6,11]],
-  numberOfDeliveries: ['BDeliveries8','BDeliveries9','BDeliveries10','BDeliveries11','BDeliveries12','BDeliveries13','BDeliveries14','BDeliveries15','BDeliveries16','BDeliveries17','BDeliveries18','BDeliveries19','BDeliveries20','BDeliveries21','BDeliveries22','BDeliveries23','BDeliveries24','BDeliveries1'],
-  recDriverNumber: ['BDrivers8','BDrivers9','BDrivers10','BDrivers11','BDrivers12','BDrivers13', 'BDrivers14','BDrivers14','BDrivers15','BDrivers16','BDrivers17','BDrivers18','BDrivers19','BDrivers20','BDrivers21','BDrivers22','BDrivers23','BDrivers24','BDrivers1']
+  minMaxDeliveries: [[0,4],[0,4],[0,4],[0,4],[0,4],[0,4],[1,4],[1,4],[1,4],[3,8],[3,8],[3,8],[5,12],[5,12],[5,12],[6,11],[6,11],[6,11]]
 }
 
 
-// //Hillsboro Object
-//
-// var Hillsboro = {
-//   minMaxPizza: [[1,3],[1,3],[1,3],[5,9],[5,9],[5,9],[2,13],[2,13],[2,13],[18,32],[18,32],[18,32],[1,3],[1,3],[1,3],[8,20],[8,20],[8,20]],
-//   pizzaSales: ['HPizzaSales8','HPizzaSales9','HPizzaSales10','HPizzaSales11','HPizzaSales12','HPizzaSales13','HPizzaSales14','HPizzaSales15','HPizzaSales16','HPizzaSales17','HPizzaSales18','HPizzaSales19','HPizzaSales20','HPizzaSales21','HPizzaSales22','HPizzaSales23','HPizzaSales24','HPizzaSales1'],
-//   minMaxDeliveries: [[1,7],[1,7],[1,7],[2,8],[2,8],[2,8],[1,6],[1,6],[1,6],[3,9],[3,9],[3,9],[5,12],[5,12],[5,12],[6,16],[6,16],[6,16]],
-//   numberOfDeliveries: ['HDeliveries8','HDeliveries9','HDeliveries10','HDeliveries11','HDeliveries12','HDeliveries13','HDeliveries14','HDeliveries15','HDeliveries16','HDeliveries17','HDeliveries18','HDeliveries19','HDeliveries20','HDeliveries21','HDeliveries22','HDeliveries23','HDeliveries24','HDeliveries1'],
-//   recDriverNumber: ['HDrivers8','HDrivers9','HDrivers10','HDrivers11','HDrivers12','HDrivers13', 'HDrivers14','HDrivers14','HDrivers15','HDrivers16','HDrivers17','HDrivers18','HDrivers19','HDrivers20','HDrivers21','HDrivers22','HDrivers23','HDrivers24','HDrivers1']
-// }
-//
-// printDatainTable(Hillsboro);
+//Hillsboro Object
+
+var Hillsboro = {
+  AnchorName: 'HillsboroAnchor',
+  timeSlots: ['8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12am','1am'],
+  minMaxPizza: [[1,3],[1,3],[1,3],[5,9],[5,9],[5,9],[2,13],[2,13],[2,13],[18,32],[18,32],[18,32],[1,3],[1,3],[1,3],[8,20],[8,20],[8,20]],
+  minMaxDeliveries: [[1,7],[1,7],[1,7],[2,8],[2,8],[2,8],[1,6],[1,6],[1,6],[3,9],[3,9],[3,9],[5,12],[5,12],[5,12],[6,16],[6,16],[6,16]]
+}
+
+//Downtown Object
+
+var Downtown = {
+  AnchorName: 'DowntownAnchor',
+  timeSlots: ['8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12am','1am'],
+  minMaxPizza: [[0,4],[0,4],[0,4],[0,7],[0,7],[0,7],[2,15],[2,15],[2,15],[10,26],[10,26],[10,26],[8,22],[8,22],[8,22],[0,2],[0,2],[0,2]],
+  minMaxDeliveries: [[0,4],[0,4],[0,4],[0,4],[0,4],[0,4],[1,4],[1,4],[1,4],[4,6],[4,6],[4,6],[7,15],[7,15],[7,15],[2,8],[2,8],[2,8]]
+}
+
 
 //Random Number Generator
 
@@ -31,16 +36,15 @@ function randomNumber(min,max)
 
 //Generate Table Function
 
-function generate_table() {
+function generate_table(obj) {
   // get the reference for the body
-  var body = document.getElementById("BeavertonAnchor");
+  var body = document.getElementById(obj.AnchorName);
 
   // creates a <table> element and a <tbody> element
   var tbl     = document.createElement("table");
   var tblBody = document.createElement("tbody");
 
   var headerRow = document.createElement("tr"); //making the Headers
-
   var headerA = document.createElement("th");
   var headerText = document.createTextNode("Time");
   headerA.appendChild(headerText);
@@ -57,31 +61,24 @@ function generate_table() {
   var headerTextD = document.createTextNode("Recommended Number of Drivers");
   headerD.appendChild(headerTextD);
   headerRow.appendChild(headerD);
-
-
-
-  tblBody.appendChild(headerRow);
-
+  tblBody.appendChild(headerRow); //Done making Headers
 
   // creating all cells
   for (var i = 0; i < 18; i++) {
     // creates a table row
     var row = document.createElement("tr");
 
-    // for (var j = 0; j < 2; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
+//Filling the slots
       var cell = document.createElement("td");
-      var cellText = document.createTextNode(Beaverton.timeSlots[i]);
+      var cellText = document.createTextNode(obj.timeSlots[i]);
       cell.appendChild(cellText);
       row.appendChild(cell);
       var cell2 = document.createElement("td");
-      var cellText2 = document.createTextNode(randomNumber(Beaverton.minMaxPizza[i][0], Beaverton.minMaxPizza[i][1]));
+      var cellText2 = document.createTextNode(randomNumber(obj.minMaxPizza[i][0], obj.minMaxPizza[i][1]));
       cell2.appendChild(cellText2);
       row.appendChild(cell2);
       var cell3 = document.createElement("td");
-      var randomDelivery = randomNumber(Beaverton.minMaxDeliveries[i][0], Beaverton.minMaxDeliveries[i][1]);
+      var randomDelivery = randomNumber(obj.minMaxDeliveries[i][0], obj.minMaxDeliveries[i][1]);
       var cellText3 = document.createTextNode(randomDelivery);
       cell3.appendChild(cellText3);
       row.appendChild(cell3);
@@ -89,8 +86,6 @@ function generate_table() {
       var cellText4 = document.createTextNode(Math.ceil(randomDelivery/3));
       cell4.appendChild(cellText4);
       row.appendChild(cell4);
-
-    //}
 
     // add the row to the end of the table body
 
@@ -105,45 +100,6 @@ function generate_table() {
   tbl.setAttribute("border", "2");
 }
 
-
-generate_table();
-
-
-
-
-//Beaverton Function to fill Table
-
-// function printDatainTable (obj) {
-//   for (i=0; i <obj.pizzaSales.length; i++){
-//     var tempID = document.getElementById(obj.pizzaSales[i]);
-//     tempRandom = randomNumber(obj.minMaxPizza[i][0], obj.minMaxPizza[i][1]);
-//     tempID.textContent = tempRandom;
-//     var tempID2 = document.getElementById(obj.numberOfDeliveries[i]);
-//       if (tempRandom > obj.minMaxDeliveries[i][1]){
-//         var tempdeliver = randomNumber(obj.minMaxDeliveries[i][0], obj.minMaxDeliveries[i][1]);
-//         tempID2.textContent = tempdeliver;
-//       } else {
-//         var tempdeliver = randomNumber(obj.minMaxDeliveries[i][0],tempRandom);
-//         tempID2.textContent = tempdeliver;
-//       }
-//
-//     var tempID3 = document.getElementById(obj.recDriverNumber[i]);
-//     var tempRandom3 = Math.ceil(tempdeliver/3);
-//     tempID3.textContent = tempRandom3;
-//   }
-// }
-//
-// printDatainTable(Beaverton);
-//
-//
-// //Hillsboro Object
-//
-// var Hillsboro = {
-//   minMaxPizza: [[1,3],[1,3],[1,3],[5,9],[5,9],[5,9],[2,13],[2,13],[2,13],[18,32],[18,32],[18,32],[1,3],[1,3],[1,3],[8,20],[8,20],[8,20]],
-//   pizzaSales: ['HPizzaSales8','HPizzaSales9','HPizzaSales10','HPizzaSales11','HPizzaSales12','HPizzaSales13','HPizzaSales14','HPizzaSales15','HPizzaSales16','HPizzaSales17','HPizzaSales18','HPizzaSales19','HPizzaSales20','HPizzaSales21','HPizzaSales22','HPizzaSales23','HPizzaSales24','HPizzaSales1'],
-//   minMaxDeliveries: [[1,7],[1,7],[1,7],[2,8],[2,8],[2,8],[1,6],[1,6],[1,6],[3,9],[3,9],[3,9],[5,12],[5,12],[5,12],[6,16],[6,16],[6,16]],
-//   numberOfDeliveries: ['HDeliveries8','HDeliveries9','HDeliveries10','HDeliveries11','HDeliveries12','HDeliveries13','HDeliveries14','HDeliveries15','HDeliveries16','HDeliveries17','HDeliveries18','HDeliveries19','HDeliveries20','HDeliveries21','HDeliveries22','HDeliveries23','HDeliveries24','HDeliveries1'],
-//   recDriverNumber: ['HDrivers8','HDrivers9','HDrivers10','HDrivers11','HDrivers12','HDrivers13', 'HDrivers14','HDrivers14','HDrivers15','HDrivers16','HDrivers17','HDrivers18','HDrivers19','HDrivers20','HDrivers21','HDrivers22','HDrivers23','HDrivers24','HDrivers1']
-// }
-//
-// printDatainTable(Hillsboro);
+generate_table(Beaverton);
+generate_table(Hillsboro);
+generate_table(Downtown);
